4bceb2483147b4218499a4e945ae59307374e151
Jonathan Yuan <jy90023@gmail.com>
2024-03-12 21:49:30 -0400

    Fix issues in PR

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ Entry point for LTE Communication.
 import pathlib
 import time
 
-import image_encode
-import socket_wrapper
-
 from modules.common.camera.modules.camera_device import CameraDevice
+from modules.common.lte.image_encode import image_encode
+from modules.common.lte import socket_wrapper
 from modules.common.mavlink.modules import flight_controller
 
 
@@ -18,9 +17,8 @@ LOG_DIRECTORY_PATH = pathlib.Path("logs")
 LOG_NAME = pathlib.Path(LOG_DIRECTORY_PATH, "image")
 DATA_COLLECTION_DELAY_TIME = 1.0  # seconds
 
-IMAGE_ENCODE_EXT = ".png"
-
-SOCKET_CREATION_MAX_ATTEMPTS = 10
+SOCKET_CREATE_MAX_ATTEMPTS = 10
+SOCKET_CONNECT_MAX_ATTEMPTS = 10
 SOCKET_HOST = "127.0.0.1"
 SOCKET_PORT = 8080
 
@@ -54,16 +52,12 @@ def main() -> int:
 
     camera = CameraDevice(0, 100, str(LOG_NAME))
 
-    s = None
-    for _ in range(0, SOCKET_CREATION_MAX_ATTEMPTS):
-        result, s = socket_wrapper.Socket.create(SOCKET_HOST, SOCKET_PORT)
-        if not result:
-            print("Failed to create Socket object.")
-        else:
-            break
+    result, socket_instance = socket_wrapper.Socket.create(
+        SOCKET_HOST, SOCKET_PORT, SOCKET_CREATE_MAX_ATTEMPTS, SOCKET_CONNECT_MAX_ATTEMPTS
+    )
 
-    if s is None:
-        print("SOCKET_CREATION_MAX_ATTEMPTS reached, unable to create Socket object.")
+    if not result:
+        print("Max attempts reached, could not create or connect socket.")
         return -1
 
     while True:
@@ -72,12 +66,12 @@ def main() -> int:
             print("Failed to get image.")
             continue
 
-        result, image_bytes = image_encode.encode_image_as_bytes(IMAGE_ENCODE_EXT, image)
+        result, image_bytes = image_encode(image)
         if not result:
-            print(f"Failed to encode image as a {IMAGE_ENCODE_EXT}.")
+            print("Failed to encode image.")
             continue
 
-        result = s.send(image_bytes)
+        result = socket_instance.send(image_bytes)
         if not result:
             print("Failed to send image byte data over socket.")
 
